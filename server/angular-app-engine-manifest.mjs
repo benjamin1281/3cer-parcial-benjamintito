133d4db3c112fe742802a5878d6b2a0c52c52230
Benjamín1281 <benjamintito12355@gmail.com>
2025-11-20 03:06:44 -0400

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://benjamin1281.github.io/3cer-parcial-benjamintito',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
